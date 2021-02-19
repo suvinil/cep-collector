2ef2c4b3a845cc69be8f13873471cdc7125a8632
Lenilson <lenilsonmadureira@gmail.com>
2021-02-19 09:48:46 -0300

    fix(addresses migration): deleted_at field

--- src/config/database/migrations/20210219123750-addresses.js
+++ src/config/database/migrations/20210219123750-addresses.js
@@ -15,6 +15,10 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,

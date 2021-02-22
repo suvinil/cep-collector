a7e4701f7dc71ea3bf5a819f5ca5c7680149c1f9
Lenilson <lenilsonmadureira@gmail.com>
2021-02-22 14:48:14 -0300

    feat(auth address): isAuthorized addres create and list

--- src/routes/address.routes.js
+++ src/routes/address.routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { addressController } = require('../controllers');
-const { validate } = require('../middlewares');
+const { validate, isAuthorized } = require('../middlewares');
 const {
   validationSchemas: { address },
 } = require('../validations');
+
+router.use(isAuthorized);
 
 router.get('/', validate(address.list), addressController.list);
 router.post('/', validate(address.create), addressController.create);
